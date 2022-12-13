e25d34cd3f011201372bf8f864beaa37222f574f
Carlos Gabriel Godri <carlos.godri@compasso.com.br>
2022-12-13 09:36:23 -0300

    release v2.4

--- js/eventPage.js
+++ js/eventPage.js
@@ -1,14 +1,19 @@
-const apiurl = 'https://look-mac-anddrees.herokuapp.com';
+const apiurl = 'https://gist.githubusercontent.com/godrix/480f09ee8c0ed8f76df1dee6b9b3ebf5/raw/5374329f0d5ea739ddae1275da0f86c38ea3cb9d/lookup-mac-anddress';
 
 const lookupMacAndress = {
     getMAC: async (MACAddress) => {
         if(lookupMacAndress.isValidInputValue(MACAddress)){
             return 'Invalid Format';
         }
-        const response = await fetch(`${apiurl}/${MACAddress}`);
-        const {vendor} = await response.json();
-        
-        return vendor;
+        const response = await fetch(`${apiurl}`);
+       
+       const macClear = MACAddress.replace(/\D/g, "").substring(0, 6)
+       
+       const responseJson = await response.json();
+   
+       const match = responseJson.find(item => item.mac === macClear)
+   
+       return match.vendor;
     },
     isValidInputValue:(mac)=>{
         const ismac = lookupMacAndress.regexMACAdress(mac);
